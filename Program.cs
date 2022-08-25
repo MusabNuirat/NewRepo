04a9ad7a4bffcc4ad50ef92164c167f52122d7bd
MusabNuirat <103640603+MusabNuirat@users.noreply.github.com>
2022-08-25 21:38:43 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -13,13 +12,6 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
 
 app.Run();
